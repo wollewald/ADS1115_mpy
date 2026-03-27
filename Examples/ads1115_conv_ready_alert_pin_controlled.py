daf4e515ffbd333f09bb22acb353395d7504e033
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2026-03-27 21:27:22 +0100

    Refactor ADS1115 example for improved clarity

--- Examples/ads1115_conv_ready_alert_pin_controlled.py
+++ Examples/ads1115_conv_ready_alert_pin_controlled.py
@@ -18,7 +18,7 @@ from ADS1115 import *
 
 ADS1115_ADDRESS = 0x48
 
-i2c = I2C(0)
+i2c = I2C(0, scl=Pin(22), sda=Pin(21))
 adc = ADS1115(ADS1115_ADDRESS, i2c=i2c)
 
 alertPin = Pin(17, Pin.IN, Pin.PULL_DOWN)
